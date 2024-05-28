2d3914c4c6e04efe8b99ebedcbbf15e676ff0245
Samm <sanjuana.macias@cimat.mx>
2024-05-27 20:29:00 -0600

    Cambie algo

---
+++ karma.conf.js
@@ -0,0 +1,40 @@
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      //require('karma-safari-launcher'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage-istanbul-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      clearContext: false 
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true 
+    },
+    coverageIstanbulReporter: {
+      dir: require('path').join(__dirname, './coverage/my-app'),
+      reports: ['html', 'lcovonly', 'text-summary'],
+      fixWebpackSourcePaths: true
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    //browsers: ['Safari'], 
+    browsers: ['ChromeHeadlessNoSandbox'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-setuid-sandbox']
+      }
+    },
+    singleRun: false,
+    restartOnFileChange: true
+  });
+};
